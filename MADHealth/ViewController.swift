f226d937358f3637ff5273ca2b11af48af889f55
Nick Poe <n.poluektau@yandex.by>
2021-11-13 08:19:15 +0300

    first commit

---
+++ MADHealth/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MADHealth
+//
+//  Created by Nick Poe on 13.11.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
